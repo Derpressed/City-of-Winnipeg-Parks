0d88f81ca1e7128b9de71d9074e9e275e00580d0
Christian <christianppasco@gmail.com>
2026-05-24 02:30:00 -0500

    added WPG park API

--- scripts/dataFetch.js
+++ scripts/dataFetch.js
@@ -3,16 +3,25 @@ function onMarkerClick(park) {
     map.setView(park.coordinate, 16);
 }
 
-fetch("parkData/parks.json")
+// fetch("parkData/parks.json")
+//     .then((response) => response.json())
+//     .then((parks) => {
+//         for (let i = 0; i < parks.length; i++) {
+        
+//             let marker = L.marker(parks[i].coordinate).addTo(map);
+//             marker.bindPopup(`<b>${parks[i].name}</b>`, {autoClose: false, closeOnClick: false}).openPopup();
+        
+//             marker.on("click", () => {
+//                 onMarkerClick(parks[i])
+//             });
+//         }
+//     })
+
+fetch("parkData/parks_and_data.geojson")
     .then((response) => response.json())
-    .then((parks) => {
-        for (let i = 0; i < parks.length; i++) {
-        
-            let marker = L.marker(parks[i].coordinate).addTo(map);
-            marker.bindPopup(`<b>${parks[i].name}</b>`, {autoClose: false, closeOnClick: false}).openPopup();
-        
-            marker.on("click", () => {
-                onMarkerClick(parks[i])
-            });
+    .then(parks => {
+        for (let i = 0; i < parks.features.length; i++) {
+            console.log(parks.features[i]);
+            L.geoJSON(parks.features[i]).addTo(map);
         }
     })
